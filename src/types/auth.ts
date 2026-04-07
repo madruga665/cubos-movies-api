5552c7a43a4f26efdea957446065def09b91791b
madruga665 <madruga665@hotmail.com>
2026-04-07 05:55:51 -0300

    chore: add auth middleware

---
+++ src/types/auth.ts
@@ -0,0 +1,20 @@
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  emailVerified: boolean;
+  image?: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type Session = {
+  id: string;
+  expiresAt: Date;
+  token: string;
+  createdAt: Date;
+  updatedAt: Date;
+  ipAddress?: string | null;
+  userAgent?: string | null;
+  userId: string;
+};
