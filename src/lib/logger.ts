85a149c5a2132c575cb568b5e611f7626aee4dee
madruga665 <madruga665@hotmail.com>
2026-04-15 12:11:21 -0300

    fix: logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -4,15 +4,21 @@ import LokiTransport from 'winston-loki';
 const logger = winston.createLogger({
   transports: [
     new LokiTransport({
-      basicAuth: `${process.env.LOKI_USER_ID}:${process.env.LOKI_TOKEN}`,
       host: process.env.LOKI_BASE_URL || 'http://localhost:3100',
+      basicAuth: `${process.env.LOKI_USER_ID}:${process.env.LOKI_TOKEN}`,
       labels: { app: 'cubos-movies-api' },
       json: true,
+      batching: true,
+      interval: 5,
       replaceTimestamp: true,
+      format: winston.format.json(),
       onConnectionError: (err) => console.error('Erro ao conectar ao Loki', err),
     }),
     new winston.transports.Console({
-      format: winston.format.simple(),
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
     }),
   ],
 });
